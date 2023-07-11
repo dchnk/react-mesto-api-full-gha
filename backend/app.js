9b0eec7e540f261a1f05458a5e29f5d9f6751b25
dchnk <seroxe2@yandex.ru>
2023-07-12 03:35:37 +0700

    feat: crash-test

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,13 @@ app.use(cors({
 }));
 
 app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(router);
 app.use(errorLogger);
 app.use(errors());
